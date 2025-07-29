2e0c7b347961bd0628a70ad4f36481f6c0b99880
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-07-29 18:25:31 -0300

    Update: Criando a classe do Layout do Jogo e setando as palavras do game

--- src/view/JogoView.java
+++ src/view/JogoView.java
@@ -1,6 +1,33 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 
 public class JogoView extends JFrame {
+
+    //Criando as Váriaveis Iniciais do APP
+    private final GradeDePalavaras gradeDePalavaras;
+    private JTable tabela;
+    private DefaultTableCellRenderer[][] renderers;
+    private Point pontoInicial, pontoFinal;
+    private JLabel dicas;
+
+
+    //Classe Principal
+    public JogoView(){
+        //Set dos atributos da Tela
+        setTitle("Caça-Palavras");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600,600);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        //Set das palavras que vão aparecer no cabeçalho do APP e no Caça Palavras
+        String[] palavras = {"PALMERIAS","BOTAFOGO","FLAMENGO","SANTOS","CRUZEIRO","GREMIO"};
+        this.gradeDePalavaras = new GradeDePalavras(palavras);
+        montarInterface();
+        setVisible(true);
+
+    }
 }
